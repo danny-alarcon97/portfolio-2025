3e8028c0a3571cd6af868e080ed6c44e45709a1f
Daniel Alarcon <danny.alarcon1997@gmail.com>
2025-01-10 17:55:33 -0500

    show my work button & basic nav

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,5 @@
+import { FaLocationArrow } from "react-icons/fa";
+import MagicButton from "./ui/MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
@@ -28,10 +30,20 @@ const Hero = () => {
 
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless Experiences"
+            words="Transforming Concepts into Seamless User Experiences"
           />
 
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl"></p>
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-blue-100">
+            Hi, I&apos;m Daniel, a Junior Fullstack Developer based in Virginia.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
